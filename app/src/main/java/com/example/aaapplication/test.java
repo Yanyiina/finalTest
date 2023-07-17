3b83c82507773cf8713be4633fc0dbc634e25c3e
tc <2686156576@qq.com>
2023-07-17 20:57:53 +0800

    test1

---
+++ app/src/main/java/com/example/aaapplication/test.java
@@ -0,0 +1,11 @@
+package com.example.aaapplication;
+
+/**
+ * @PROJECT_NAME: finalTest
+ * @DESCRIPTION:
+ * @USER: Administrator
+ * @DATE: 2023/7/17 20:56
+ */
+public class test {
+    private int my;
+}
