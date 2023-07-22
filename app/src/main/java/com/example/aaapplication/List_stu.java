7a9cc4ba26f97f1c9917cec51249a8a360edfce6
tc <2686156576@qq.com>
2023-07-22 08:46:55 +0800

    0719-afternoon-fitness

--- app/src/main/java/com/example/aaapplication/List_stu.java
+++ app/src/main/java/com/example/aaapplication/List_stu.java
@@ -13,6 +13,62 @@ public class List_stu  implements Serializable {
     // 身高 体重
     private Double height;
     private Double weight;
+    // 保存正面体态识别结果
+    private String positive_points;
+    private String positive_height;
+    private String positive_img;
+    // 保存侧面体资体态相关结果
+    private String side_img;
+    private String side_height;
+    private String side_points;
+
+    public String getPositive_points() {
+        return positive_points;
+    }
+
+    public void setPositive_points(String positive_points) {
+        this.positive_points = positive_points;
+    }
+
+    public String getPositive_height() {
+        return positive_height;
+    }
+
+    public void setPositive_height(String positive_height) {
+        this.positive_height = positive_height;
+    }
+
+    public String getPositive_img() {
+        return positive_img;
+    }
+
+    public void setPositive_img(String positive_img) {
+        this.positive_img = positive_img;
+    }
+
+    public String getSide_img() {
+        return side_img;
+    }
+
+    public void setSide_img(String side_img) {
+        this.side_img = side_img;
+    }
+
+    public String getSide_height() {
+        return side_height;
+    }
+
+    public void setSide_height(String side_height) {
+        this.side_height = side_height;
+    }
+
+    public String getSide_points() {
+        return side_points;
+    }
+
+    public void setSide_points(String side_points) {
+        this.side_points = side_points;
+    }
 
     public Double getHeight() {
         return height;
@@ -50,7 +106,7 @@ public class List_stu  implements Serializable {
         this.gender = gender;
     }
 
-    public List_stu(Integer ID, String child_name, Integer type, Integer gender, Integer age, Double height, Double weight, String parent_phone, String tag_id_list, String[] tag_list) {
+    public List_stu(Integer ID, String child_name, Integer type, Integer gender, Integer age, Double height, Double weight, String positive_points, String positive_height, String positive_img, String side_img, String side_height, String side_points, String parent_phone, String tag_id_list, String[] tag_list) {
         this.ID = ID;
         this.child_name = child_name;
         this.type = type;
@@ -58,6 +114,12 @@ public class List_stu  implements Serializable {
         this.age = age;
         this.height = height;
         this.weight = weight;
+        this.positive_points = positive_points;
+        this.positive_height = positive_height;
+        this.positive_img = positive_img;
+        this.side_img = side_img;
+        this.side_height = side_height;
+        this.side_points = side_points;
         this.parent_phone = parent_phone;
         this.tag_id_list = tag_id_list;
         this.tag_list = tag_list;
--- app/src/main/java/com/example/aaapplication/Main4_sub3.java
+++ app/src/main/java/com/example/aaapplication/Main4_sub3.java
@@ -1,25 +1,78 @@
 package com.example.aaapplication;
 
 import android.content.Intent;
+import android.hardware.Camera;
+import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.Environment;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+
+import static com.example.utils.OssUploader.uploadImageToOSS;
 
 
-
-public class Main4_sub3 extends AppCompatActivity {
+public class Main4_sub3 extends AppCompatActivity implements SurfaceHolder.Callback {
     private ProgressBar progressBar;
     private TextView timeText;
     private CountDownTimer countDownTimer;
+
+    private String back[];
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    //
+    private SurfaceView mSurfaceView;
+    private Camera mCamera;
+    private CountDownTimer mCountDownTimer;
+    private SurfaceHolder mSurfaceHolder;
+
+    private String positive_img;
+    private String positive_points = null;
+    private String positive_heights = null;
+//    private TextView tvCountdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4_sub3);
+
+        /**
+         * @description:
+         * @param savedInstanceState:
+         * @return
+         * @author: Tian
+         */
+        //
+        mSurfaceView = findViewById(R.id.surfaceView);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        takePhotoWithDelay(5);
+
+        //
+        /**
+         * @description:
+         * @param savedInstanceState:
+         * @return
+         * @author: Tian
+         */
 
         Intent intent = getIntent();
         final List_stu student = (List_stu) intent.getSerializableExtra("student");
@@ -44,11 +97,11 @@ public class Main4_sub3 extends AppCompatActivity {
         progressBar.setMax(60);
 
         // 创建并启动倒计时定时器
-        countDownTimer = new CountDownTimer(6000, 1000) {
+        countDownTimer = new CountDownTimer(10000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
                 // 每秒更新进度条和时间文本
-                int progress = (int) (60000 - millisUntilFinished) / 1000;
+                int progress = (int) (10000 - millisUntilFinished) / 1000;
                 progressBar.setProgress(progress);
                 timeText.setText(formatTime(millisUntilFinished));
             }
@@ -69,8 +122,153 @@ public class Main4_sub3 extends AppCompatActivity {
 
         // 启动倒计时定时器
         countDownTimer.start();
+    }
 
-        
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mCamera==null){
+            mCamera=getCamera();//初始化mCamera
+            if(mSurfaceHolder!=null){
+                setStartPreview(mCamera,mSurfaceHolder);//开始预览，这样打开这个activity就直接显示预览画面
+            }
+        }
+    }
+    //预览
+    private void setStartPreview(Camera camera,SurfaceHolder holder){
+        try {
+            camera.setPreviewDisplay(holder);//绑定camera和holder
+            camera.setDisplayOrientation(90);//系统camera预览角度默认为横屏，旋转90°可变为竖屏
+            camera.startPreview();//开始在Surfaceview组件中预览camera获取到的内容
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //打开相机
+    private Camera getCamera(){
+        Camera camera;
+        try {
+            camera=Camera.open(0);//获取camera对象
+        }catch (Exception e){
+            camera=null;//发生异常将camera=null
+        }
+        return camera;
+    }
+
+    //activity生命周期结束
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();
+    }
+
+    //停止相机预览，释放相机资源
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);//将摄像头的回调置空，取消mcamera和mholder的关联
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        setStartPreview(mCamera,mSurfaceHolder);//开始预览
+    }
+
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mCamera.stopPreview();//停止取景
+        setStartPreview(mCamera,mSurfaceHolder);//开始预览
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        releaseCamera();
+    }
+
+    private static File savePhoto(byte[] data) {
+        // 创建保存图片的目录
+        File pictureDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyPictures");
+
+        if (!pictureDirectory.exists()) {
+            pictureDirectory.mkdirs();
+        }
+        // 格式化日期作为图片文件名
+        String fileName = "IMG_text.png";
+
+        // 创建图片文件
+        File pictureFile = new File(pictureDirectory, fileName);
+        System.out.println("imagePath路径："+pictureFile);
+
+        try {
+            // 保存图片数据到文件
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            fos.write(data);
+            fos.close();
+            Log.i("Camera", "图片保存成功: " + pictureFile.getAbsolutePath());
+        } catch (Exception e) {
+            Log.e("Camera", "图片保存失败", e);
+        }
+        return pictureFile;
+    }
+    private void takePhotoWithDelay(int delaySeconds) {
+        mCountDownTimer = new CountDownTimer(delaySeconds * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                // 更新倒计时显示，例如在界面上显示秒数
+//                int secondsRemaining = (int) millisUntilFinished / 1000;
+//                // 这里你需要根据自己的实际情况更新界面上的倒计时显示
+//                tvCountdown.setText(String.valueOf(secondsRemaining));
+            }
+            @Override
+            public void onFinish() {
+                takePictureAndStopCamera();
+            }
+        };
+        mCountDownTimer.start();
+    }
+    private void takePictureAndStopCamera() {
+        mCamera.takePicture(null, null,
+                new Camera.PictureCallback() {
+                    @Override
+                    public void onPictureTaken(byte[] data, Camera camera) {
+                        // 处理照片保存逻辑
+                        savePhoto(data);
+                        upload(data);
+
+                        //倒计时停止
+                        mCountDownTimer.cancel();
+                        // 相机预览
+                        camera.startPreview();
+                    }
+                });
+    }
+    /**
+     //     * 上传照片
+     //     */
+    public void upload(final byte[] data) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                String imagePath = "/storage/emulated/0/Pictures/MyPictures/IMG_20230708_143644.png";
+//                uploadImageToOSS(imagePath);
+                String imagePath = String.valueOf(savePhoto(data));
+                // uploadImageToOSS(imagePath);
+                CompletableFuture<String[]> future = uploadImageToOSS(imagePath);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    future.thenAccept(result -> {
+                        if(result != null){
+                            back = result;
+                            System.out.println("+++++1" + back[1]);
+                        }
+                    });
+                }
+
+
+            }
+        }).start();
 
     }
 
@@ -90,4 +288,8 @@ public class Main4_sub3 extends AppCompatActivity {
         return String.format("%02d:%02d", minutes, seconds);
     }
 
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
 }
